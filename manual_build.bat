43594e935eb20a296ecc58625385174ae9642700
stefand799 <dinustefan1996@yahoo.com>
2025-12-07 02:40:54 +0200

    Created tests for the newly implemented strategy design pattern

--- manual_build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building BlackjackUI...
-cd /d D:\ISProjectV2\out\build\x64-Debug
-cmake --build . --target BlackjackUI --config Debug -v
-echo.
-echo Build complete. Check output above for errors.
-pause
